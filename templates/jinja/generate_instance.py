92732e7fb5b477d5999b120ebdaad5cb78051d0b
Giorgos Dimitriou <george.dimitriou@cloudreach.com>
2017-07-01 12:47:38 +0100

    Small changes Clearing jenkins userdata

--- templates/jinja/generate_instance.py
+++ templates/jinja/generate_instance.py
@@ -15,22 +15,12 @@ class CreateInstance():
         content = yaml.load(config)
 
     new_dict = {}
-    new_dict['tags'] = content['sceptre_user_data']
-
-    for rule in range(len(rules)):
-        new_dict['rules']["rule{}".format(rule)] = {
-            'protocol' : content['sceptre_user_data']['sec_group']['ingress'][rule].split(' ')[0],
-            'ip' : content['sceptre_user_data']['sec_group']['ingress'][rule].split(' ')[1],
-            'from' : int(content['sceptre_user_data']['sec_group']['ingress'][rule].split(' ')[2]),
-            'to' : int(content['sceptre_user_data']['sec_group']['ingress'][rule].split(' ')[-1])
-         }
+    new_dict = content['sceptre_user_data']
 
 
     def print_temp(self):
         template = self.ENV.get_template("templates/poc.yaml")
         return template.render(config=self.new_dict)
-
-
 
 
 def sceptre_handler(sceptre_user_data):
